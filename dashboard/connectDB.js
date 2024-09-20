b62646e75de7df86ba91411b723348ac9888917f
CJ Valdez <166489707+CJCameHome@users.noreply.github.com>
2024-09-20 08:33:21 +0000

    Inang goatbot na to

--- dashboard/connectDB.js
+++ dashboard/connectDB.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-const dirConfig = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'config.dev.json' : 'config.json'}`);
+const dirConfig = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 
+'config.json' :									'config.json'}`);
 const dirConfigCommands = path.join(`${__dirname}/../${process.env.NODE_ENV === 'development' ? 'configCommands.dev.json' : 'configCommands.json'}`);
 
 global.GoatBot = {
